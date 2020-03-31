db3740b52b7ead0bccdb8e5142fb25feb7e6537a
wrongreflection <crunkinbuck@gmail.com>
2020-03-31 14:14:37 -0400

    created auto mmap functionality for assemble

--- assemble.py
+++ assemble.py
@@ -1,7 +1,11 @@
 import pwndbg
+import gdb
 import argparse
-from keystone import *
+import os
+os.environ['PWNLIB_NOTERM'] = 'true'
+from pwn import asm,context
 
+# TODO: include a jmp patching option... maybe too much
 parser = argparse.ArgumentParser(description="Starting at the specified address, write the assembled instructions")
 parser.add_argument("address", type=int, help="The address to start assembling at")
 parser.add_argument("code", type=str, default='', nargs='?', help="The code to be assembled")
@@ -10,12 +14,17 @@ parser.add_argument("code", type=str, default='', nargs='?', help="The code to b
 def assemble(address, code):
     # Get the current architecture from pwndbg
     # Currently only supports i386 and x86_64
-    if pwndbg.arch.current == 'i386':
-        ks = Ks(KS_ARCH_X86, KS_MODE_32)
-    elif pwndbg.arch.current == 'x86-64':
-        ks = Ks(KS_ARCH_X86, KS_MODE_64)
-    else:
-        raise NotImplementedError(f"Unimplemented architecture: {pwndbg.arch.current}")
+    try:
+        context.update(arch=pwndbg.arch.current, os='linux')
+    except:
+        raise NotImplementedError("Unimplemented architecture: {pwndbg.arch.current}")
+
+    # Use 0 as an address arg to create a new space
+    if not pwndbg.memory.poke(address):
+        if address == long(str(gdb.parse_and_eval('mmap(0x%x, 0x400, 7, 0x32, -1, 0)' % address)), 16):
+            print("mmap'd at address 0x%x" % address)
+        else:
+            raise MemoryError("Cannot mmap at the address 0x%x" % address)
 
     # Allow entering assembly a line at a time until "end"
     if not code:
@@ -30,8 +39,8 @@ def assemble(address, code):
     # Assemble the instructions
     # the asm function returns a tuple with the 
     # bytes being a list of ints at index 0
-    asm = bytes(ks.asm(code)[0])
+    asm_str = bytes(asm(code))
     
     # Write the assembled code to the specified address
-    pwndbg.memory.write(address, asm)
+    pwndbg.memory.write(address, asm_str)
 
